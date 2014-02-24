4131f1a66f3e3b0415ff9ee0ca88962b1cebb055
soathiam <soathiam@msn.com>
2014-02-24 13:30:50 -0500

    Update index.php
    Changes:
    - Added user role concept
    - Echoed it

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-echo "Hello World!"
+$userRoles = array(
+    'John'=>'Geek',
+    'Harry'=>'CEO',
+    'Lea'=>'CFO',
+    'Pushkibak'=>'PMO',
+  );
+foreach($userRoles as $name=>$role)
+{
+  echo "Hello $name, have you done everything a $role should do today?<br/>"
+}
+
 
 ?>
